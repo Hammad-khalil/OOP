67c897a1c08579a3c821dd4997d1f55c8d338568
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-11-16 20:20:43 +0500

    Code Complted with Comments Added

--- MultiLvlInheritance.java
+++ MultiLvlInheritance.java
@@ -1,15 +1,25 @@
 package OOP;
 
+/*
+  This is an Example of Multi Level Inheritance,
+  where Circle inherits properties of Form and then Rectangle inherits properties
+  of Circle which was inherited by Circle from Form itself :>
+*/
 class Form {
     String color;
     String type;
 
-    public void FormDisplay() {
-        System.out.println("Color of this Form is : " + this.color + " Type of this Form is : " + this.type);
+    public void FormDisplay(String name) {
+        System.out
+                .println(name + ":   Color of this Form is : " + this.color + " Type of this Form is : " + this.type);
     }
 }
 
 class Circle extends Form {
+
+}
+
+class Rectangle extends Circle {
 
 }
 
@@ -20,6 +30,10 @@ public class MultiLvlInheritance {
         Circle myobj = new Circle();
         myobj.color = "Purple";
         myobj.type = "2D";
-        myobj.FormDisplay();
+        myobj.FormDisplay("Circle");
+        Rectangle recobj = new Rectangle();
+        recobj.color = "Blue";
+        recobj.type = "quadrilateral";
+        recobj.FormDisplay("Rectangle");
     }
 }
