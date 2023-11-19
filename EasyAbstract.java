354fddeb4f57d5261d26b4b609eb2dbb66fc9c78
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-11-20 04:51:23 +0500

    Abstraction With Keyword

---
+++ EasyAbstract.java
@@ -0,0 +1,10 @@
+package OOP;
+
+/**
+ * EasyAbstract
+ */
+public class EasyAbstract {
+    public static void main(String[] args) {
+        System.out.println("Using Abstraction with Abstract Keyword");
+    }
+}
