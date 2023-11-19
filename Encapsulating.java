ebd7038089a7040b01c412a615d326996b8ce56f
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-11-20 04:30:15 +0500

    Encapsulation program but with some error now , will fix and update later

--- Encapsulating.java
+++ Encapsulating.java
@@ -1,7 +1,24 @@
 package OOP;
 
+class Car {
+    private String wheel;
+    // private String Engine;
+    // private Boolean Light;
+
+    public String getwheel() {
+        return this.wheel;
+    }
+
+    public void setwheel(String name) {
+        this.wheel = name;
+    }
+}
+
 public class Encapsulating {
     public static void main(String[] args) {
         System.out.println("Starting Encapsulation");
+        Car mywheel = new Car();
+        mywheel.setwheel("Continental Tyres");
+        System.out.println("The Wheel is" + mywheel.getwheel());
     }
 }
