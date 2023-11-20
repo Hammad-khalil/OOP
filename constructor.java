e4c2c8dc5414645858a8592299cfc6cef9ed65ae
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-11-21 04:51:17 +0500

    Constructor program Done

--- constructor.java
+++ constructor.java
@@ -10,7 +10,7 @@ class Person {
     }
 
     public void displaypersons() {
-        System.out.println("Person Name : " + this.name + " Age of Person " + this.age + "\n");
+        System.out.println("Person Name : " + this.name + "\n" + "Age of Person " + this.age + "\n");
     }
 }
 
@@ -18,5 +18,9 @@ public class constructor {
 
     public static void main(String[] args) {
         System.out.println("Constructor making it easier for us to initialize");
+        Person person1 = new Person("Imran Khan", 45);
+        person1.displaypersons();
+        Person person2 = new Person("Pat Cummins", 30);
+        person2.displaypersons();
     }
 }
