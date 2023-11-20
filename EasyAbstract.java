e63713b0fca8aaaeafad960cfe6c042160be5412
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-11-20 05:21:34 +0500

    Abstraction Program Completed with simple Keyword

--- EasyAbstract.java
+++ EasyAbstract.java
@@ -1,10 +1,39 @@
 package OOP;
 
-/**
- * EasyAbstract
- */
+abstract class Animal {
+    abstract void walk();
+
+    Animal() {
+        System.out.println("Contructor of Abstract class");
+    }
+
+    public void myfunc() {
+        System.out.println("We can create Constructor of Abstract class and even create normal functions inside it ");
+    }
+}
+
+class Penguin extends Animal {
+    public void walk() {
+        System.out.println("Walking on 2 legs");
+
+    }
+
+    Penguin() {
+        System.out.println("Constructor of Penguin");
+    }
+}
+
+class Goat extends Animal {
+    public void walk() {
+        System.out.println("Walking on 4 legs");
+    }
+}
+
 public class EasyAbstract {
     public static void main(String[] args) {
-        System.out.println("Using Abstraction with Abstract Keyword");
+        Goat goat = new Goat();
+        goat.walk();
+        Penguin peng = new Penguin();
+        peng.walk();
     }
 }
