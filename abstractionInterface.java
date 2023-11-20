515347ea320cd771c22eb4d06b119932e10d6877
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-11-20 05:30:10 +0500

    Abstraction code Template using Interface

---
+++ abstractionInterface.java
@@ -0,0 +1,8 @@
+package OOP;
+// Abstraction with Interface
+
+public class abstractionInterface {
+    public static void main(String[] args) {
+        System.out.println("Abstraction using Interface");
+    }
+}
